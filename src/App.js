ef25356c935b387799871ca1c12cd6bbd9d0187e
Louis Gourain <louis.gourain@gmail.com>
2019-09-28 17:00:32 +0200

    adding form input component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
 
 const HatsPage = () => {
@@ -21,6 +22,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/hats" component={HatsPage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
