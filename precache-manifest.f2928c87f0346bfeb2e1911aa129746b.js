0202e247f091ecd9f5b03bb8de3711a644b6b801
Louis Gourain <lgourain@wanadev.fr>
2021-02-01 20:01:09 +0100

    Updates

---
+++ precache-manifest.f2928c87f0346bfeb2e1911aa129746b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef14d8ed445fb188229fca1f3f2c609",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "9fded633f7b096ed0b49",
+    "url": "/crwn-clothing/static/css/main.a3ccdd86.chunk.css"
+  },
+  {
+    "revision": "682eec5ec9a91dffdbbe",
+    "url": "/crwn-clothing/static/js/2.49e841f7.chunk.js"
+  },
+  {
+    "revision": "9fded633f7b096ed0b49",
+    "url": "/crwn-clothing/static/js/main.e6f2f4fa.chunk.js"
+  },
+  {
+    "revision": "c122b15bd1a9273f1b4d",
+    "url": "/crwn-clothing/static/js/runtime-main.5cd65c40.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
